ecfe0683f8890594998cab102ef0b9d676b571e5
dragonempire96 <maximilian.mleziva@gmx.de>
2025-04-20 10:40:23 +0200

    feat: added Flyway Migration scripts

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,38 @@
+create table if not exists activity (
+    id serial primary key,
+    uuid uuid,
+    sport uuid,
+    km double,
+    date date,
+    name uuid
+);
+
+create table if not exists users (
+    id serial primary key,
+    uuid uuid,
+    username varchar(255),
+    name varchar(255),
+    team uuid
+);
+
+create table if not exists teams (
+    id serial primary key,
+    uuid uuid,
+    name varchar(255),
+    "year" date
+);
+
+create table if not exists sport (
+    id serial primary key,
+    uuid uuid,
+    sport varchar(255),
+    price double,
+    category varchar(255),
+    active boolean
+);
+
+create table if not exists hash_data (
+    id serial primary key,
+    hash int,
+    inserted boolean
+);
