3bc64b379228727f125cc3e08d3510ca55dc9a19
dragonempire96 <maximilian.mleziva@gmx.de>
2025-04-20 04:18:39 +0200

    test: refactored parameters to the test class

--- src/test/java/de/gooddragon/jederkilometer/JederKilometerApplicationTests.java
+++ src/test/java/de/gooddragon/jederkilometer/JederKilometerApplicationTests.java
@@ -4,7 +4,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = {"spring.datasource.url=jdbc:h2:mem:strava",
+        "spring.datasource.username=test",
+        "spring.datasource.password=test",
+        "jederKilometer.rollen.admin=dragonempire96",
+        "jederKilometer.strava.client-id=1234546",
+        "jederKilometer.strava.client-secret=123STRAVA_CLIENT_SECRET",
+        "jederKilometer.strava.refresh-token=STRAVA_REFRESH_TOKEN",
+        "jederKilometer.strava.clubId=1234556",
+        "jederKilometer.strava.clubPage=1",
+        "jederKilometer.strava.pageEntries=1"})
 @ActiveProfiles({"test"})
 class JederKilometerApplicationTests {
 
